9c7eb824a1a17dd5d44eb150394dec0c6700cf21
cgswong <cgs.wong@gmail.com>
2014-10-16 21:51:56 -0500

    Added update script, modified install to suite, customized plist.

--- brewupdate-install.sh
+++ brewupdate-install.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+# ############################################################################
+# NAME: brewupdate-install.sh
+# DESC: Script to setup launchd process to update, upgrade and check (doctor)
+#       for brew.
+#
+# LOG:
+# yyyy/mm/dd [user] [version]: [notes]
+# 2014/10/15 cgwong v0.1.0: Initial creation from https://github.com/mkalmes/brewupdate/blob/develop/brewupdate-install.sh
+# ############################################################################
+
 set -e
+
 AGENTS="$HOME/Library/LaunchAgents"
-PLIST="$AGENTS/net.mkalmes.brewupdate.plist"
-REPO=${REPO:-mkalmes}
+PLIST="$AGENTS/net.brewupdate.agent.plist"
+REPO=${REPO:-cgswong}
 BRANCH=${BRANCH:-master}
-REMOTE="https://github.com/$REPO/brewupdate/raw/$BRANCH/net.mkalmes.brewupdate.plist"
+REMOTE="https://github.com/$REPO/brewupdate/raw/$BRANCH/net.brewupdate.agent.plist"
+
 [ -f "$PLIST" ] && launchctl unload "$PLIST"
-if [ "$1" == "uninstall" ]
-then
-  \rm -f "$PLIST"
-  echo "Unloaded brewupdate."
-  exit 0
+if [ "$1" == "uninstall" ]; then
+  rm -f "$PLIST"
+  if [ $? -eq 0 ]; then
+    echo "Unloaded brewupdate."
+    exit 0
+  else
+    echo "Failed unloading brewupdate!!"
+    exit 1
+  fi
 fi
+
 curl -L "$REMOTE" >| "$PLIST"
 [ -f "$PLIST" ] && launchctl load "$PLIST"
 echo "Loaded brewupdate."
